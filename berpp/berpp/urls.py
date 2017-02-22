764687e29adaabf37bf08253ed3e621b8a78f27f
Alex Hong <a.hong@berkeley.edu>
2017-02-21 20:58:40 -0800

    urls for website

--- berpp/berpp/urls.py
+++ berpp/berpp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import website
 
 urlpatterns = [
-    url(r'^/', include('website.urls')),
+    url(r'^', include('website.urls')),
     #url(r'^admin/', admin.site.urls),
 ]
