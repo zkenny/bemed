acbde157842a3a97bbaf2cb2fa42954111244165
Raymond Ng <raymondng@berkeley.edu>
2017-03-14 22:49:48 -0700

    signup function under views

--- berpp/website/views.py
+++ berpp/website/views.py
@@ -33,8 +33,27 @@ def ec_form(request):
     return render(request, 'ec_forms.html', {'form': form, 'bookinst':book_inst})
 
 def signup(request):
-    if request.method == "POST":
-        
-        return render(request, 'signup.html')
+    context = RequestContext(request)
+    registered = False
 
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print(str(user_form.errors) + " " + str(profile_form.errors))
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    return render_to_response(
+            'users/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+            context)
